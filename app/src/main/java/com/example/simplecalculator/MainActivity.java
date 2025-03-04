d2385249157213d9dd9ae16065aa16fa944a7877
OriLaufer <orilaufer15@gmail.com>
2025-03-04 22:46:45 +0200

    version 3

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,24 +1,101 @@
 package com.example.simplecalculator;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-    @SuppressLint("NewApi")
+import java.text.DecimalFormat;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    GoogleSignInClient googleSignInClient;
+    ShapeableImageView imageView;
+    TextView nameTv, mailTV;
+    TextView name, mail;
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK) {
+            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+            try {
+                GoogleSignInAccount signInAccount = accountTask.getResult(ApiException.class);
+                AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+                auth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            auth = FirebaseAuth.getInstance();
+                            Glide.with(MainActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
+                            name.setText(auth.getCurrentUser().getDisplayName());
+                            mail.setText(auth.getCurrentUser().getEmail());
+                            Toast.makeText(MainActivity.this, "Signed in successfully!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failed to sign in: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseApp.initializeApp(this);
+
+        imageView = findViewById(R.id.profileImage);
+        name = findViewById(R.id.nameTV);
+        mail = findViewById(R.id.mailTV);
+
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, options);
+
+        auth = FirebaseAuth.getInstance();
+
+        SignInButton signInButton = findViewById(R.id.signIn);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = googleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(intent);
+                // Code for sign-in action goes here
+            }
+        });
         // Apply window insets for edge-to-edge display
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, windowInsets) -> {
             final android.graphics.Insets systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).toPlatformInsets();
@@ -27,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @SuppressLint("SetTextI18n")
     public void onBtnClicked(View view) {
         EditText num1EditText = findViewById(R.id.Num1);
         EditText num2EditText = findViewById(R.id.Num2);
@@ -48,24 +124,36 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Invalid input. Please enter numbers only.", Toast.LENGTH_SHORT).show();
             return;
         }
-        Integer result = null;
 
-        if (view.getId() == R.id.btnPlus)
-            result = num1 + num2;
-        if (view.getId() == R.id.btnMinus)
-            result = num1 - num2;
-        if (view.getId() == R.id.btnMult)
-            result = num1 * num2;
-        if (view.getId() == R.id.btnDiv)
-            result = num1 / num2;
+        int result = 0;
+        boolean validOperation = true;
 
-        if (result != null) {
-            TextView tvRes = findViewById(R.id.tvResult);
-            tvRes.setText(result.toString());
+        switch (view.getId()) {
+            case R.id.btnPlus:
+                result = num1 + num2;
+                break;
+            case R.id.btnMinus:
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                if (num2 == 0) {
+                    Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+                    validOperation = false;
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                validOperation = false;
         }
 
-        if (num2 == 0) {
-            Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+        if (validOperation) {
+            TextView resultTextView = findViewById(R.id.tvResult);
+            DecimalFormat decimalFormat = new DecimalFormat("#,###");
+            resultTextView.setText(decimalFormat.format(result));
         }
     }
 }
